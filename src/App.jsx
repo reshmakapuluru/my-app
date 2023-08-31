03da7c5605a145c3d2bcdde8ad450f2932adad70
reshmakapuluru <kapulurureshma024@gmail.com>
2023-08-31 17:49:51 +0530

    rty

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Sorting from './Sorting';
 import axios from 'axios';
-import First from './First';
-import Second from './Second';
-import Project from './Project';
 import 'bootstrap/dist/css/bootstrap.css';
 import Reviews from './Reviews'
 
@@ -13,12 +9,8 @@ function App() {
   
   return (
     <div>
-      <div className='mybox'>
-          {/* <First></First> */}
-          {/* <Second></Second> */}
-          <Project></Project>
-      </div>
-      {/* <Sorting></Sorting> */}
+      
+      
       <Reviews></Reviews>
      
       
